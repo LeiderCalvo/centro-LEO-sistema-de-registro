b2116b1cc4b22c417e1b89b5f7676c71bf4756fb
Leider Calvo <leiderstivencalvogomez@hotmail.com>
2019-06-27 12:49:17 -0500

    slider full

--- src/components/HomeAdmin.tsx
+++ src/components/HomeAdmin.tsx
@@ -6,6 +6,7 @@ import Navigation from "./monitor/Navigation";
 import DataBaseFireBase from "../utils/DataBaseFireBase";
 import Horario from "./monitor/Horario";
 import Historial from "./monitor/Historial";
+import Slider from "./Slider";
 
 @observer
 class HomeAdmin extends Component <any, any>{
@@ -36,14 +37,18 @@ class HomeAdmin extends Component <any, any>{
                     <div className="workArea uno">
                         <p className='time' style={{color: '#88b3ff'}}>{store.currentTime.split(':')[0] + ' : '+store.currentTime.split(':')[1]}</p>
                         <p className='date' style={{color: '#88b3ff'}}>{store.currentDate}</p>
+                        <Slider/>
 
-                        {store.excusas.map((elem, index)=>{
+                        {/*store.excusas.map((elem, index)=>{
                             return <div key={index+'exc'} className="excusa">
                                 <p id='fecha'>{elem.fecha+', '+DataBaseFireBase.transfomNumberToTime(elem.inicio)+' - '+DataBaseFireBase.transfomNumberToTime(elem.fin)}</p>
                                 <p>"{elem.razon}".</p>
                                 <p>({elem.monitor})</p>
                             </div>
-                        })}
+                        })
+
+                        */
+                        }
                     </div>
                 :store.navItemSelected === 'Horario'&&
                     <Horario/>
---
+++ src/components/Slider.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import DataBaseFireBase from '../utils/DataBaseFireBase';
+import store from '../stores/store';
+
+@observer
+class Slider extends React.Component<any,any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            pos:0,
+            op: 1
+        }
+    }
+
+    handleClick(val: string){
+        let temp = 0;
+        val === 'back'? temp = this.state.pos-1 : temp = this.state.pos+1;
+        if(temp < store.excusas.length && temp>=0){
+            this.setState({op: 0});
+
+            setTimeout(() => {
+                this.setState({pos: temp, op: 1});
+            }, 700);
+        }
+
+    }
+
+    render(){
+        return(
+            store.excusas[this.state.pos] !== null && store.excusas[this.state.pos] !== undefined ?
+                <div className="Slider">
+                    {store.excusas.length>1&&<button onClick={()=>this.handleClick('back')}>{'<'}</button>
+                    }
+                    <div className="excusa" style={{opacity: this.state.op}}>
+                        <p id='fecha'>{store.excusas[this.state.pos].fecha+', '+DataBaseFireBase.transfomNumberToTime(store.excusas[this.state.pos].inicio)+' - '+DataBaseFireBase.transfomNumberToTime(store.excusas[this.state.pos].fin)}</p>
+                        <p>"{store.excusas[this.state.pos].razon}".</p>
+                        <p>({store.excusas[this.state.pos].monitor})</p>
+                    </div>
+                    {store.excusas.length>1&&
+                    <button onClick={()=>this.handleClick('next')}>{'>'}</button>
+                    }
+                </div>
+            :
+                <div className="Slider"></div>
+
+        )
+    }
+}
+
+export default Slider;
