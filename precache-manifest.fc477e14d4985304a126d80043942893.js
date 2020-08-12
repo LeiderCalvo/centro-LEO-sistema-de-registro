6ccd46dbc476deb344056796010631881b14b504
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-08-11 19:28:50 -0500

    new branch to test deploy

---
+++ precache-manifest.fc477e14d4985304a126d80043942893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8543c02645a82414acfc3de7bc81d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d4676b369fdb3924f136",
+    "url": "./static/css/main.05b556ba.chunk.css"
+  },
+  {
+    "revision": "b1a9fa68c9291f988828",
+    "url": "./static/js/2.614165c1.chunk.js"
+  },
+  {
+    "revision": "d4676b369fdb3924f136",
+    "url": "./static/js/main.331458a9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
